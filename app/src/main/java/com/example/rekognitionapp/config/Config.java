1036f7e73c847bf3667d811a54d7afed9c6e52e8
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-08-07 19:13:34 +0900

    Delete Config.java

--- app/src/main/java/com/example/rekognitionapp/config/Config.java
+++
@@ -1,8 +0,0 @@
-package com.example.rekognitionapp.config;
-
-public class Config {
-
-    public static String aws_access_key_id = "AKIAZTQXQZNSYLTDVYD6";
-    public static String aws_secret_access_key = "FL9vjKp9xver+elk+4BoJ/bNuqxX9yG2Wc/k1oRp";
-
-}
